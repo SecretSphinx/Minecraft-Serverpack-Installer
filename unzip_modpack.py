be36a21ce6b07d6f45a51d086d07f3adf2948dbe
SecretSphinx <150881093+SecretSphinx@users.noreply.github.com>
2023-12-18 05:00:27 +0000

    Update unzip_modpack.py

--- unzip_modpack.py
+++ unzip_modpack.py
@@ -1,17 +1,28 @@
 import os
-import pathlib
 import shutil
+import pathlib
+import re
+
+
+def sanitize_folder_name(name):
+    # Replace spaces and unwanted characters with underscores
+    sanitized = re.sub(r"[^\w\-_\.]", "_", name)
+    # Remove trailing special characters like '!'
+    sanitized = re.sub(r"[_!@#$%^&*()+=]+$", "", sanitized)
+    return sanitized
+
 
 def unzip(zip_name, modpack_name, file_ext, this_dir, output=False):
-
     if output:
-        extract_dir = os.path.join(this_dir, output)
+        folder_name = sanitize_folder_name(output)
     else:
-        extract_dir = os.path.join(this_dir, modpack_name.replace(":", "_").replace(" ", "_").replace(",", ""))
+        folder_name = sanitize_folder_name(modpack_name)
+
+    extract_dir = os.path.join(this_dir, folder_name)
     my_zip = os.path.join(this_dir, zip_name)
     shutil.unpack_archive(my_zip, extract_dir)
     print("Extraction Done, deleting zip")
     os.remove(my_zip)
-    #oschmod.set_mode_recursive(extract_dir, "777")
+    # oschmod.set_mode_recursive(extract_dir, "777")
     path = pathlib.PurePath(extract_dir)
     return path.name
